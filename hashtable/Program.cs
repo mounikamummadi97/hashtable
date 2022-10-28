c31c1566907dcbfd15755058a52026e4c52552c8
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 16:19:32 +0530

    Add project files.

---
+++ hashtable/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections;
+namespace demo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("092", "tom");
+            ht.Add("012", "jerry"); 
+           //
+           //
+           ICollection key =ht.Keys;
+            foreach(string k in key)
+            {
+                Console.WriteLine(k+":" + ht[k]);
+            }
+        }
+    }
+}
